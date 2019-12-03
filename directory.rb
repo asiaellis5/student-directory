3be7187e6d9bcb2f01aaff10d1c8a174e8fdbc6a
Asia <asiaellis5@gmail.com>
2019-12-03 16:42:10 +0000

    centered the output

--- directory.rb
+++ directory.rb
@@ -5,40 +5,42 @@ def input_students
      puts "Would you like to add a new person yes or no, if no hit return twice."
         answer = gets.chomp
         if !answer.empty?
-        puts "what is the name of the person?"
-        name = gets.chomp
+          puts "what is the name of the person?"
+          name = gets.chomp
     
-        puts "What is that persons hobby?"
-        hobby = gets.chomp
+          puts "What is that persons hobby?"
+          hobby = gets.chomp
     
-        puts "Where were they born?"
-        country = gets.chomp
+          puts "Where were they born?"
+          country = gets.chomp
     
-        puts "what cohort are they in?"
-        cohort = gets.chomp
+          puts "what cohort are they in?"
+          cohort = gets.chomp
         
-        students << {name: name, hobby: hobby, country_of_birth: country, cohort: cohort}
+          students << {name: name, hobby: hobby, country_of_birth: country, cohort: cohort}
+          puts "Now we have #{students.count} students"
         else 
           break
         end
      end
      students
+     
 end 
 
 def print_header
-    puts "The students of Villains Academy".center(50)
-    puts "--------------".center(50)
+    puts "The students of Villains Academy".center(100)
+    puts "--------------".center(100)
 end
 def print(students)
   count = 0
   until count == students.length
-    puts "#{students[count][:name]}, #{students[count][:hobby]}, #{students[count][:country_of_birth]} (#{students[count][:cohort]} cohort)".center(50)
+    puts "name : #{students[count][:name]}".center(25) + " hobby : #{students[count][:hobby]}".center(25) + " country : #{students[count][:country_of_birth]}".center(25) + " (#{students[count][:cohort]} cohort)".center(25)
     count +=1
   end
 end
 
 def print_footer(names)
-    puts "Overall, we have #{names.count} great students".center(50)
+    puts "Overall, we have #{names.count} great students".center(100)
 end
 #nothing happens until call the methods
 students = input_students
