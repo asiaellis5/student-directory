16a907853145a531f26e651d5251b26995b7d86c
Asia <asiaellis5@gmail.com>
2019-12-03 18:04:57 +0000

    ask both name and cohort, added  default values and corrected  typos

--- directory.rb
+++ directory.rb
@@ -1,5 +1,21 @@
 def input_students
     students = []
+
+    months = {
+        "" => :none,
+        "January" => :January,
+        "February" => :February,
+        "March" => :March,
+        "April" => :April,
+        "May" => :May,
+        "June" => :June,
+        "July" => :July,
+        "August" => :August,
+        "September" => :September,
+        "October" => :October,
+        "November" => :November,
+        "December" => :December
+        }
     
      while true do
      puts "Would you like to add a new person yes or no, if no hit return twice."
@@ -7,17 +23,19 @@ def input_students
         if !answer.empty?
           puts "what is the name of the person?"
           name = gets.chomp
-    
-          puts "What is that persons hobby?"
-          hobby = gets.chomp
-    
-          puts "Where were they born?"
-          country = gets.chomp
-    
+            if name.empty?
+                name = "none"
+            end
+
           puts "what cohort are they in?"
-          cohort = gets.chomp
+          cohort = months[gets.capitalize.chomp]
+            while cohort == nil do
+                puts "Try again: what cohort are they in?"
+                cohort = months[gets.capitalize.chomp]
+            end
+
         
-          students << {name: name, hobby: hobby, country_of_birth: country, cohort: cohort}
+          students << {name: name, cohort: cohort}
           puts "Now we have #{students.count} students"
         else 
           break
@@ -28,19 +46,19 @@ def input_students
 end 
 
 def print_header
-    puts "The students of Villains Academy".center(100)
-    puts "--------------".center(100)
+    puts "The students of Villains Academy".center(50)
+    puts "--------------".center(50)
 end
 def print(students)
   count = 0
   until count == students.length
-    puts "name : #{students[count][:name]}".center(25) + " hobby : #{students[count][:hobby]}".center(25) + " country : #{students[count][:country_of_birth]}".center(25) + " (#{students[count][:cohort]} cohort)".center(25)
+    puts "Name : #{students[count][:name]}".center(25) + "Cohort: #{students[count][:cohort]}".center(25)
     count +=1
   end
 end
 
 def print_footer(names)
-    puts "Overall, we have #{names.count} great students".center(100)
+    puts "Overall, we have #{names.count} great students".center(50)
 end
 #nothing happens until call the methods
 students = input_students
