ee86e40002c1dfd5f7cab6bacc8128cdfa006ff4
Asia <asiaellis5@gmail.com>
2019-12-06 15:39:25 +0000

    ruby quine

---
+++ ruby_quine.rb
@@ -0,0 +1,8 @@
+eval hello="print 'eval hello=';p hello"
+
+eval helloworld="print 'eval helloworld=';p helloworld"
+
+a = <<ASIA
+puts("a = <<ASIA", a, ASIA, a")
+ASIA
+puts("a = <<ASIA", a, "ASIA", a)
